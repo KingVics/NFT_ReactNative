af19780fbef574b0587a1b6bc044229150a2801e
Victor <okoromivictorsunday@gmail.com>
2022-08-01 23:07:07 +0100

    initial commit

---
+++ components/Index.js
@@ -0,0 +1,14 @@
+import FocusStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NTFCard from "./NTFCard";
+import { CircleButton, RectButton } from "./Buttons";
+import { SubInfo } from "./SubInfo";
+
+export {
+  FocusStatusBar,
+  HomeHeader,
+  NTFCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+};
---
+++ screens/Home.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { View, SafeAreaView, FlatList, Text } from "react-native";
+import FocusedStatusBar from "../components/FocusedStatusBar";
+import HomeHeader from "../components/HomeHeader";
+import NTFCard from "../components/NTFCard";
+
+import { COLORS, NFTData } from "../constants";
+
+const Home = () => {
+    const [nftData, setNftData] = useState(NFTData);
+
+    const handleSearch = (value) => {
+      if (value.length === 0) {
+        setNftData(NFTData);
+      }
+
+      const filteredData = NFTData.filter((item) =>
+        item.name.toLowerCase().includes(value.toLowerCase())
+      );
+
+      if (filteredData.length === 0) {
+        setNftData(NFTData);
+      } else {
+        setNftData(filteredData);
+      }
+    };
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar backgroundColor={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={nftData}
+            renderItem={({ item }) => <NTFCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
+          />
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
